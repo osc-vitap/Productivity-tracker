0e0e039a1613220d017f39efd305dfee5ab5fef6
Vineet Tiwari <iamvineettiwari012@gmail.com>
2020-12-12 20:39:11 +0530

    Boilerplate for django project

---
+++ ProductivityTracker/ProductivityTracker/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ProductivityTracker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProductivityTracker.settings')
+
+application = get_wsgi_application()
