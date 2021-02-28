801886d00834874c5d6d9f9f2007e7a08a078519
vijay <vijaybalajitheboss@gmail.com>
2021-02-28 18:34:20 +0530

    Updated variable names and reformated code

--- Scripts/scheduler.py
+++ Scripts/scheduler.py
@@ -3,22 +3,27 @@ import time
 from datetime import datetime as d
 import os
 import ctypes
-import psutil    
+import psutil
 
 # Hosts path of Windows. change hosts path according to your OS
 hosts_path = "C:/Windows/System32/drivers/etc/hosts"
 # localhost's IP
 redirect = "127.0.0.1"
 # add website to websites list to block
-websites = ["www.facebook.com","www.instagram.com", "www.gmail.com"]
-applications = ["notepad.exe","firefox.exe"]
+websites = ["www.facebook.com", "www.instagram.com", "www.gmail.com"]
+applications = ["notepad.exe", "firefox.exe"]
 
-def focus(startH,startM,endH,endM):
+
+def focus(startH, startM, endH, endM):
     while True:
         # time of your work
-        if d(d.now().year, d.now().month, d.now().day, startH, startM) < d.now() < d(d.now().year, d.now().month, d.now().day,endH,endM):
+        if (
+            d(d.now().year, d.now().month, d.now().day, startH, startM)
+            < d.now()
+            < d(d.now().year, d.now().month, d.now().day, endH, endM)
+        ):
             print("Working hour...", d.now())
-            with open(hosts_path, 'r+') as file:
+            with open(hosts_path, "r+") as file:
                 content = file.read()
                 for website in websites:
                     if website in content:
@@ -27,10 +32,12 @@ def focus(startH,startM,endH,endM):
                         # mapping hostnames to your localhost IP address
                         file.write(redirect + " " + website + "\n")
             for application in applications:
-                if(application in (p.name() for p in psutil.process_iter())):
-                    os.system('taskkill /im '+application) # select process by its name
+                if application in (p.name() for p in psutil.process_iter()):
+                    os.system(
+                        "taskkill /im " + application
+                    )  # select process by its name
         else:
-            with open(hosts_path, 'r+') as file:
+            with open(hosts_path, "r+") as file:
                 content = file.readlines()
                 file.seek(0)
                 for line in content:
@@ -39,8 +46,10 @@ def focus(startH,startM,endH,endM):
                 # removing hostnmes from host file
                 file.truncate()
             print("Chill time", d.now())
-            
+
         time.sleep(2)
+
+
 def job():
     print("Work mode on...")
     print("TO ENABLE THE FOCUS MODE\n___________________________")
@@ -51,7 +60,7 @@ def job():
     focus(startH, startM, endH, endM)
 
 
-#schedule.every(1).to(2).seconds.do(job)
+# schedule.every(1).to(2).seconds.do(job)
 schedule.every().day.at("01:01").do(job)
 
 while True:
